16a859d5949bda4976a44ed286517812e5844fcf
GUSTAVO DE OLIVEIRA PEREIRA <21193137@CFP127ALUNO.LOCAL>
2021-09-20 08:32:10 -0300

    Tela de Login concluida

---
+++ app/src/main/java/com/example/projeto_imc/ProfileActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.projeto_imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile2)
+    }
+}
