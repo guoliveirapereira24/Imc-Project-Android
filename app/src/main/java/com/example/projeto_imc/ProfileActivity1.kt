e9a6f41454236001a5691c11abb785609bec78aa
GUSTAVO DE OLIVEIRA PEREIRA <21193137@CFP127ALUNO.LOCAL>
2021-09-27 11:27:09 -0300

    Aula dia 27/09/2021

---
+++ app/src/main/java/com/example/projeto_imc/ProfileActivity1.kt
@@ -0,0 +1,11 @@
+package com.example.projeto_imc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile1)
+    }
+}
